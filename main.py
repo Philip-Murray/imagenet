395b10cf436f477e18563795f888bfbe39e311b5
Philip Murray <philippaulmurray@gmail.com>
2021-08-11 03:25:52 +0200

    Serialization with Inheritance test

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
 
 
 import sys, os
-import loader, features, persistence
+import loader, features, persistence, pickle
+
+
+
+def saveprogress(x):
+    pickle_out = open("dict.pickle","wb")
+    pickle.dump(x, pickle_out)
+    pickle_out.close()
+    
+
+def loadprogress():
+    pickle_in = open("dict.pickle", "rb")
+    x = pickle.load(pickle_in)
+    return x
+
+
+class A:
+    def hi(self):
+        print("HI")
+
+    def bye(self):
+        print("bye")
+
+class B(A):
+    def bye(self):
+        print("B")
+
+def t1():
+    u = B()
+    u.hi()
+    u.bye()
+
+    saveprogress(u)
+
+def t2():
+    u = loadprogress()
+    u.hi()
+    u.bye()
+    super(type(u), u).bye()
+
+t2()
+
+quit()
 
 A4_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,7 +61,7 @@ def LoadImages(force_not_present=False, save_copy_if_not_present=True):
         if save_copy_if_not_present:
             persistence.savedatabase()
 
-LoadImages()
+#LoadImages()
 
 
 def LoadSession(force_not_present=False):
@@ -31,6 +73,8 @@ def LoadSession(force_not_present=False):
 def SaveSession():
     persistence.saveprogress(g_params.session, g_params.session_filename)
 
+import model
+model.go()
 
 print(len(sys.argv))
 
--- model.py
+++ model.py
@@ -1,8 +1,68 @@
 
 
 
+    
 
 class Model:
 
 
-    def forwardpass():
+
+    def __init__(self, model, X_set, Y_set):
+        self.model = model
+        self.X_set = X_set
+        self.Y_set = Y_set
+
+    def fit(X_set, Y_set, epochs, report_progress=True):
+        pass
+
+    def accuracy_test(X_set, Y_set, report_progress=True):
+        pass
+
+    def predict_batch(X_set, Y_set, report_progress=True):
+        pass
+
+    def predict(X_set, Y_set, report_progress=True):
+
+    
+
+
+
+
+
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+def go():
+    import time
+
+    # A List of Items
+    items = list(range(0, 57))
+    l = len(items)
+
+    # Initial call to print 0% progress
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    for i, item in enumerate(items):
+        # Do stuff...
+        time.sleep(0.1)
+        # Update Progress Bar
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
